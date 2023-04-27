49f2301ca86a387231b62d67bf4a67afda8ddb75
shaniet <sshanietalor@gmail.com>
2023-04-26 21:12:47 -0700

    updated files

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ const uploadedImage = document.getElementById('uploaded-image');
 const captionElement = document.getElementById("caption");
 const spotifyElement = document.getElementById("spotify");
 const titleElement = document.getElementById("title");
-
+const { promisify } = require('util');
+const fs = require('fs');
+const convert = require('heic-convert');
 const uploadPhotoLabel = document.querySelector('label[for="upload-photo"]');
 
 const captions = [
   
-  "I woke up like this",
-  "violet fishy"
+  " I woke up like this",
+ 
  
 ];
 
@@ -19,21 +21,47 @@ let isFirstUpload = true;
 // Add event listener for when file is uploaded
 uploadPhotoButton.addEventListener('change', function() {
   const file = this.files[0];
+
   const reader = new FileReader();
 
-  reader.addEventListener('load', function() {
+  reader.addEventListener('load', async function() {
+
+    let uploadedImageNew = new Image();
+
+     // Convert HEIC format to JPEG
+  if (file && file.type === 'image/heic') {
+    try {
+      const jpegBlob = await window.heicConvert({
+        blob: file,
+        toType: "image/jpeg"
+      });
+      file = new File([jpegBlob], `${file.name.slice(0, -5)}.jpg`, {type: 'image/jpeg'});
+    } catch (error) {
+      console.error("HEIC conversion failed:", error);
+      return;
+    }
+  }
+
+      const url = URL.createObjectURL(file);
+
+      uploadedImageNew.src = url;
+
+
     // create new image element for uploaded image
-    const uploadedImageNew = new Image();
-    uploadedImageNew.src = reader.result;
+
+    // uploadedImageNew.src = reader.result;
     
     // create new image element for fishbowl image
-    const fishbowlImage = new Image();
-    fishbowlImage.src = "fishbowl.png";
+    const borderImg = new Image();
+    borderImg.src = "images/border.png";
     const randomIndex = Math.floor(Math.random() * captions.length);
 
     titleElement.textContent = "if i were a were a fish and you caught me you'd say" +  captions[randomIndex];
 
-    uploadedImage.src = fishbowlImage.src;
+    uploadedImage.src = borderImg.src;
+
+
+    
 
     uploadedImage.style.backgroundImage = `url('${uploadedImageNew.src}')`;
 
@@ -49,6 +77,11 @@ uploadPhotoButton.addEventListener('change', function() {
       uploadedImage.classList.add("circle");
       isFirstUpload = false;
     }
+
+       // revoke the object URL
+    uploadedImageNew.onload = function() {
+      URL.revokeObjectURL(url);
+    };
   }, false);
 
   if (file) {
