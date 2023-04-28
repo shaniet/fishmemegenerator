a532fb774295bcba397e7aa0444a239fad44cf5b
shaniet <sshanietalor@gmail.com>
2023-04-28 12:17:13 -0700

    blah

--- script.js
+++ script.js
@@ -9,9 +9,57 @@ const uploadPhotoLabel = document.querySelector('label[for="upload-photo"]');
 const downloadElement = document.getElementById('download');
 
 const captions = [
-"looks like i found a catch of the day!", "i think i just landed a fish-tastic prize!", "well, well, well...what do we have here?", "i'm the fish that got away...until now!", "i'm officially off the hook!", "you really scaled up with this one!", "now that's what i call a fish story!", "i guess i'm the catch of the day!", "looks like i'm taking the bait this time!", "looks like i'm in deep water now!", "are you sure you're not a cat? you're a great fisherman!", "you're really making a splash with this catch!", "i'm sure glad i'm not a mermaid!", "you know what they say...there are plenty of fish in the sea, but you caught the best one!", "looks like you're the master baiter!", "i guess i'm a little fish in a big pond!", "i guess it's time for me to scale back.", "well, looks like i'm a little hooked on you too!", "looks like i'm fresh out of luck!", "i'm a fish, not a miracle worker!", "looks like i'm your catch of the day...again!", "you're a real catch, and so am i!", "i'm floundering over here!", "you're really going fishing for compliments with this one!", "looks like you're fishing for a good time, and you found it!", "you must have used some good bait to catch me!", "i'm the fish that got caught...or was it the other way around?", "looks like i'm your lucky catch!", "looks like i'm the fish that got fried!", "i guess i'm not as slippery as i thought!", "looks like i'm the one who's hooked now!", "you're really making a splash with this catch of the day!", "you're really going overboard with this catch!", "looks like i'm on the hook for a little while!", "looks like i'm a little fish in a big fisherman's net!", "you really know how to reel 'em in!", "looks like i'm the one who's all wet now!", "you're really making waves with this catch!", "looks like i'm all out of bait!", "you're really fishing for compliments now, aren't you?", "looks like you're the one who's hooked on me!", "i guess i'm the one who's in deep water now!", "looks like i'm the one who's getting caught up in your net!", "you're really making a splash with this fish tale!", "looks like i'm the one who's on the line now!", "i'm the one who's really fishing for a compliment now!", "looks like you're the one who's swimming upstream now!", "i'm the one who's feeling a little fishy now!", "looks like i'm the one who's caught up in your web!", "you're really making a splash with this big catch!", "looks like i'm the one who's hooked on your fishing skills!", "i guess i'm the one who's all wet now!", "you're really making waves with this catch of the day!", "looks like i'm the one who's on the line now!", "you're really fishing for compliments with this one!", "looks like i'm the one who's all washed up!", "i'm the one who's really fishing for a good time now!", "looks like i'm the one who's caught your eye!", "you're really making a splash with this fish tale!", "looks like i'm the one who's in a net of trouble now!"
-]
-
+"that's one fishy fish!",
+"you reely caught me!",
+"i'm a keeper for sure!",
+"looks like you're a pro!",
+"i'm quite a catch, eh?",
+"you really hooked me!",
+"well, well, well... fish you!",
+"i'm the big one today!",
+"you netted a winner!",
+"that's a whopper catch!",
+"i'm a fin-tastic catch!",
+"you got me, hook, line and sinker!",
+"i'm a slippery little devil!",
+"looks like i'm on the menu tonight!",
+"i guess i'm a goner now!",
+"you caught me fair and square!",
+"i'm your catch of the day!",
+"you're a master angler!",
+"i'm feeling a bit baited!",
+"that's a fishin' miracle!",
+"you're really casting a wide net!",
+"i'm quite the catch, aren't i?",
+"looks like you're a lucky fisherman!",
+"i'm the one that got away... until now!",
+"you caught me red-fin'd!",
+"i'm the perfect catch for you!",
+"you're quite the fish whisperer!",
+"looks like i'm a star catch!",
+"i'm all caught up in your net!",
+"that's a reel-y impressive catch!",
+"you must have used some good bait!",
+"i'm quite the slippery fish!",
+"looks like i'm your lucky catch!",
+"i'm a fish out of water now!",
+"you're a real catch, just like me!",
+"looks like i'm all washed up!",
+"you're really fishing for compliments!",
+"i'm hooked on you now!",
+"looks like i'm on the hook for a while!",
+"you're making a splash with this catch!",
+"i'm feeling a bit fishy now!",
+"looks like you're fishing for trouble!",
+"you're really reeling them in!",
+"i'm a fish of many talents!",
+"looks like i'm quite the surprise catch!",
+"you're quite the fish catcher!",
+"i'm the catch you've been waiting for!",
+"looks like you're hooked on fishing!",
+"you're really making waves with me!",
+"i'm the fish that got caught... or did i?",
+];
 
 
 let isFirstUpload = true;
@@ -22,9 +70,16 @@ uploadPhotoButton.addEventListener('change', function() {
 
   const reader = new FileReader();
 
+
   reader.addEventListener('load', async function() {
+
+
     var myCanvas = document.getElementById('canvas');
+
     var ctx = myCanvas.getContext('2d');
+    ctx.resetTransform();
+
+  
     let uploadedImageNew = new Image();
 
     const borderImg = new Image();
@@ -44,11 +99,12 @@ uploadPhotoButton.addEventListener('change', function() {
       );
 
       // Add text above the uploaded image
-      ctx.font = "bold 24px Arial";
+      ctx.font = "bold 23px arial";
       ctx.textAlign = "center";
- 
-      ctx.fillText("if i were a fish and you caught me you'd say ", canvas.width / 2, 60);
-      ctx.fillText(captions[randomIndex], canvas.width / 2, 80);
+      const randomIndex = Math.floor(Math.random() * captions.length);
+
+      ctx.fillText("if i were a fish and you caught me you'd say ", canvas.width / 2, 55);
+      ctx.fillText(captions[randomIndex], canvas.width / 2, 95);
 
       borderImg.onload = function() {
         ctx.drawImage(borderImg,
@@ -60,8 +116,8 @@ uploadPhotoButton.addEventListener('change', function() {
       };
     };
 
+  
 
-    const randomIndex = Math.floor(Math.random() * captions.length);
 
 
     // Increase font size on first upload
@@ -82,6 +138,8 @@ uploadPhotoButton.addEventListener('change', function() {
   }
 
 });
+
+
 
 function downloadImage() {
       var link = document.createElement('a');
